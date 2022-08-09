a07779b103c47cdc20f4ed03f5b37303adf6747b
Simon he <13917107460@163.com>
2022-08-10 01:35:25 +0800

    feature: run child project scripts

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## ccommand
-用命令选择需要执行的script
+用命令选择需要执行的script, 支持直接选择子目录下的script来执行
+
 
 ## 安装
 ```bash
@@ -7,4 +8,10 @@ npm i -g ccommand
 ```
 
 ## 使用
+- ccommand # 默认当前目录
+
 ![img](./assets/kv.jpg)
+
+- ccommand ./playground # 执行子目录 playground 下的 script
+
+![img](./assets/kv1.jpg)
--- src/ccommand.ts
+++ src/ccommand.ts
@@ -1,7 +1,8 @@
 import child_process from 'child_process'
 import { getPkg } from 'simon-js-tool'
 export async function ccommand() {
-  const { scripts } = await getPkg('./package.json')
+  const dirname = process.argv[2] || '.'
+  const { scripts } = await getPkg(`${dirname}/package.json`)
   const val = child_process.spawnSync(`gum choose ${Object.keys(scripts).reduce((result, key) => {
     const value = scripts[key]
     result += `"${key}: ${value}" `
@@ -12,7 +13,7 @@ export async function ccommand() {
     encoding: 'utf8',
   }).output[1] as string
 
-  child_process.spawnSync(`npm run ${transformScripts(val)}`, {
+  child_process.spawnSync(`npm run ${transformScripts(val)} --prefix ${dirname}`, {
     shell: true,
     stdio: 'inherit',
   })

5148ac517856d85afc6e222a5e76cac2d6912a56
Simon He <13917107469@163.com>
2022-11-01 09:52:54 +0800

    test: test

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -7,7 +7,7 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
-
+// test
 export default defineConfig({
   base: './',
   resolve: {
